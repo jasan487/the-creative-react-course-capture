e9008e5b49a7e151e9db5941ef9a2a679bcfaeac
{name} <you@example.com>
2026-02-14 13:46:11 +0530

    4.Faq Section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>
+                Any questions <span>FAQ</span>
+            </h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vero, nostrum.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vero, nostrum.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Differnt payments Methods?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vero, nostrum.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer </h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vero, nostrum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FaqSection;
